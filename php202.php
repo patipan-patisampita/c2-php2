b47473ccab811dd2824e1c76f016f37eed4a8755
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-12 11:25:57 +0700

    update

--- php202.php
+++ php202.php
@@ -1,3 +1,24 @@
-<?php
-    echo "My Website";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf8">
+    <meta name="viewport" content="device=width-device, initial-scale=1.0">
+    <title>PHP: Hypertext Preprocessor.</title>
+</head>
+
+<body>
+    <h1>Welcome to php</h1>
+    <hr />
+
+    
+        <?php 
+            echo "<h4>Hello World!</h4>"; 
+            echo "My Name is Mar sakaberg";
+            echo "<br />";
+            print("I Love Face Book");
+        ?>
+    
+</body>
+
+</html>
